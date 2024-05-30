06a42febfe0387966a5f07ec6c28b147889a78ec
frojak4 <Frojak4@gmail.com>
2024-05-30 14:44:14 +0200

    Initial commit

---
+++ BettingApp/Bet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingApp
+{
+    internal class Bet
+    {
+        private string _name;
+        private int _chance;
+        private int _maxcount;
+        public int Startnumber;
+
+        public Bet(string name, int chance, int maxcount)
+        {
+            _name = name;
+            _chance = chance;
+            _maxcount = maxcount;
+        }
+
+        public void Go()
+        {
+            Startnumber = 0;
+            Random random = new Random();
+            int randomNumber = random.Next(Startnumber, _maxcount);
+        }
+    }
+}
---
+++ BettingApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BettingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = new Start();
+            start.Run();
+        }
+    }
+}
